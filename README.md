8c736bb78f560a848efad89d5c5147b9c099c581
Lautaro Demonte <lauty.d.p@gmail.com>
2022-12-14 17:01:57 -0300

    primer commit con copia de la primera entrega del proyecto

---
+++ README.md
@@ -0,0 +1,13 @@
+# Entrega 9 Lautaro Demonte
+
+## Configuracion
+
+Hacer npm install con la consola para instalar las dependencias necesarias para el funcionamiento del programa.
+
+### Ejecutar programa
+
+Hacer en la consola:
+
+```sh
+npm run dev
+```
---
+++ public/routers/productos.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+const Contenedor = require("../../controllers/controller.js");
+const productos = new Contenedor("./controllers/productos.json");
+const notFound = { error: "Producto no encontrado" };
+
+/* ok: 200
+   created: 201
+   no content: 204
+   bad request: 400
+   not found: 404
+   internal server error: 500
+    */
+
+router.get("/", async (req, res) => {
+    const arrayProductos = await productos.getAll();
+    console.log(arrayProductos);
+    res.render("productos", {
+        productos: arrayProductos,
+        style: "productos.css",
+        title: "Productos con Handlebars",
+    });
+});
+
+module.exports = router;
